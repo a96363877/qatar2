9d98e21c58892821439f4e9ca919749e5b47fbd3
a96363877 <a96363877@gmail.com>
2025-01-11 07:55:03 -0800

    sa

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -16,15 +16,13 @@ import { addData } from "../actions/adddata"
 
 const firebaseConfig = {
   // Your Firebase configuration object goes here
-  // You should replace this with your actual Firebase config
-  apiKey: "AIzaSyB1Tpv9S00TO__RCkAN95ydnMQDR7yEb0A",
-  authDomain: "csa3-e2b6a.firebaseapp.com",
-  databaseURL: "https://csa3-e2b6a-default-rtdb.firebaseio.com",
-  projectId: "csa3-e2b6a",
-  storageBucket: "csa3-e2b6a.firebasestorage.app",
-  messagingSenderId: "328650323342",
-  appId: "1:328650323342:web:468ea6435238c0452be0df",
-  measurementId: "G-D32GDGT38Q"
+  apiKey: "AIzaSyBoqC6S8FvbWhud_PnD6IC4pF61iffe4tI",
+  authDomain: "qatar22-d0e6a.firebaseapp.com",
+  projectId: "qatar22-d0e6a",
+  storageBucket: "qatar22-d0e6a.firebasestorage.app",
+  messagingSenderId: "349776944758",
+  appId: "1:349776944758:web:6a8dd2558e8ac248570957",
+  measurementId: "G-079WEHH5KL"
 }
 
 const app = initializeApp(firebaseConfig)
