d1eecc13bf0a59acd8d6800350dcb6afa6af6c2a
a96363877 <a96363877@gmail.com>
2025-01-24 07:41:05 -0800

    XZV

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -16,13 +16,13 @@ import { addData } from "../actions/adddata"
 
 const firebaseConfig = {
   // Your Firebase configuration object goes here
-  apiKey: "AIzaSyBoqC6S8FvbWhud_PnD6IC4pF61iffe4tI",
-  authDomain: "qatar22-d0e6a.firebaseapp.com",
-  projectId: "qatar22-d0e6a",
-  storageBucket: "qatar22-d0e6a.firebasestorage.app",
-  messagingSenderId: "349776944758",
-  appId: "1:349776944758:web:6a8dd2558e8ac248570957",
-  measurementId: "G-079WEHH5KL"
+  apiKey: "AIzaSyAdE8-9VwRjiqbKSImyUPu3hugol5jEMWA",
+  authDomain: "ssdd-21562.firebaseapp.com",
+  projectId: "ssdd-21562",
+  storageBucket: "ssdd-21562.firebasestorage.app",
+  messagingSenderId: "884523274527",
+  appId: "1:884523274527:web:96d8f5c87cc5bfbe3a602a",
+  measurementId: "G-0DPHLEMEBZ"
 }
 
 const app = initializeApp(firebaseConfig)
